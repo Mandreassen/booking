fea6ac96d322b12972fe1b4fc1caee19c7a635fa
Marius Andreassen <post@mandreassen.no>
2016-07-18 14:42:29 +0200

    added persons change in check-inn

--- arctic_seasport_admin/arctic_seasport_admin/Fast_check_in.Designer.cs
+++ arctic_seasport_admin/arctic_seasport_admin/Fast_check_in.Designer.cs
@@ -33,22 +33,26 @@
             this.saveButton = new System.Windows.Forms.Button();
             this.detailsButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.p_count = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.p_count)).BeginInit();
             this.SuspendLayout();
             // 
             // roComboBox
             // 
             this.roComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.roComboBox.FormattingEnabled = true;
-            this.roComboBox.Location = new System.Drawing.Point(12, 68);
+            this.roComboBox.Location = new System.Drawing.Point(12, 89);
             this.roComboBox.Name = "roComboBox";
-            this.roComboBox.Size = new System.Drawing.Size(316, 28);
+            this.roComboBox.Size = new System.Drawing.Size(258, 28);
             this.roComboBox.TabIndex = 0;
             // 
             // saveButton
             // 
             this.saveButton.BackColor = System.Drawing.SystemColors.Window;
             this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveButton.Location = new System.Drawing.Point(126, 114);
+            this.saveButton.Location = new System.Drawing.Point(133, 141);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(110, 39);
             this.saveButton.TabIndex = 1;
@@ -60,7 +64,7 @@
             // 
             this.detailsButton.BackColor = System.Drawing.SystemColors.Window;
             this.detailsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.detailsButton.Location = new System.Drawing.Point(172, 12);
+            this.detailsButton.Location = new System.Drawing.Point(198, 12);
             this.detailsButton.Name = "detailsButton";
             this.detailsButton.Size = new System.Drawing.Size(157, 39);
             this.detailsButton.TabIndex = 2;
@@ -71,19 +75,51 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.Size = new System.Drawing.Size(120, 31);
             this.label1.TabIndex = 3;
             this.label1.Text = "Check in";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Persons";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Object";
+            // 
+            // p_count
+            // 
+            this.p_count.BackColor = System.Drawing.SystemColors.Info;
+            this.p_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p_count.Location = new System.Drawing.Point(292, 90);
+            this.p_count.Name = "p_count";
+            this.p_count.Size = new System.Drawing.Size(63, 26);
+            this.p_count.TabIndex = 7;
             // 
             // Fast_check_in
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(341, 165);
+            this.ClientSize = new System.Drawing.Size(367, 192);
+            this.Controls.Add(this.p_count);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.detailsButton);
             this.Controls.Add(this.saveButton);
@@ -94,6 +130,9 @@
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Check in";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Fast_check_in_FormClosing);
+            this.Load += new System.EventHandler(this.Fast_check_in_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p_count)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,5 +144,8 @@
         private System.Windows.Forms.Button saveButton;
         private System.Windows.Forms.Button detailsButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown p_count;
     }
 }
--- arctic_seasport_admin/arctic_seasport_admin/Fast_check_in.cs
+++ arctic_seasport_admin/arctic_seasport_admin/Fast_check_in.cs
@@ -13,17 +13,39 @@ namespace arctic_seasport_admin
     public partial class Fast_check_in : Form
     {
         private string blid;
+        private Database_adapter adapter;
 
         public Fast_check_in(string booking_line)
         {
             blid = booking_line;
             InitializeComponent();
+            adapter = new Database_adapter();
+            
+        }
+
+        private void Fast_check_in_Load(object sender, EventArgs e)
+        {
             fill_Box();
+            fill_Persosn();
+        }
+
+        private void fill_Persosn()
+        {
+            var num = adapter.get_Value(string.Format(@"
+                select persons 
+                from bookings
+                where bid = (
+                    select bid from booking_lines
+                    where blid = {0}
+                );
+            ", blid));
+
+            p_count.Value = Int32.Parse(num);
         }
 
         private void fill_Box()
         {
-            var objects = Database.get_DataSet(string.Format(@"
+            var objects = adapter.get_DataSet(string.Format(@"
                 select name
                 from rent_objects
                 natural join rent_object_types
@@ -42,17 +64,22 @@ namespace arctic_seasport_admin
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            var query = string.Format("update rent_objects set currentUser = {0} where Name = '{1}';", blid, roComboBox.SelectedValue);
-            Database.set(query);
+            adapter.set(string.Format("update rent_objects set currentUser = {0} where Name = '{1}';", blid, roComboBox.SelectedValue));
+            adapter.set(string.Format("update bookings set persons = {0} where bid = (select bid from booking_lines where blid = {1});", p_count.Value, blid));
             this.Close();
         }
 
         private void detailsButton_Click(object sender, EventArgs e)
         {
-            string bid = Database.get_Value(String.Format("select bid from booking_lines where blid = {0};", blid));
+            string bid = adapter.get_Value(String.Format("select bid from booking_lines where blid = {0};", blid));
             var report = Report.booking_Confirmation(Int32.Parse(bid));
             var viewer = new ReportViewer(report);
             viewer.ShowDialog();
+        }
+
+        private void Fast_check_in_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            adapter.close();
         }
     }
 }
